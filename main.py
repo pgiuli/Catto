461f4195d5eb1e562f03737d0a5b8f44f10e88de
Pau Giuli <76668405+spxrked@users.noreply.github.com>
2022-09-29 17:03:13 +0200

    First commit for 1.1.3

---
+++ main.py
@@ -0,0 +1,87 @@
+import keyboard
+from requests import get as urlget
+import os
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+import dotenv
+
+import subrredit_pics
+
+
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+
+def save_image():
+    
+    link = subrredit_pics.get_image()
+    
+
+    r = urlget(link)
+    
+    with open('data/currentpic.jpg',"wb") as f:
+        f.write(r.content)
+    
+
+
+
+def make_good_res():
+
+    image = Image.open('data/currentpic.jpg')
+
+    width, height = image.size
+    #print(width, height)
+
+    while width > 700 or height > 700:
+        width = round(width * 0.8)
+        height = round(height * 0.8)
+    
+    #print(width, height)
+
+    resized_res = width, height
+
+    resized = image.resize(resized_res, Image.Resampling.LANCZOS)
+
+    return resized, resized_res
+
+
+def display_window(good_img, good_res):
+
+
+    good_width, good_height = good_res
+
+    window = Tk()
+
+    window.title("Sporko Catto!!!")
+    window.configure(width=good_width, height=good_height)
+    window.configure(bg='lightgray')
+    window.resizable(False, False)
+
+
+    image = ImageTk.PhotoImage(good_img)
+
+    label1 = tkinter.Label(image=image)
+    label1.image = image
+
+
+    label1.place(x=0, y=0)
+    window.mainloop()
+
+
+
+def run():
+
+    image, good_res = make_good_res()
+
+    display_window(image, good_res)
+
+    save_image()
+
+save_image()
+
+
+keyboard.add_hotkey(os.getenv("cat_hotkey"), run)
+
+
+keyboard.wait(os.getenv("stop_hotkey"))
---
+++ subrredit_pics.py
@@ -0,0 +1,146 @@
+from cgitb import enable
+import praw
+import os
+from random import randint
+import time
+import dotenv
+
+os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
+
+
+subreddit = 'blurrypicturesofcats'
+
+cache_file = 'data/'+subreddit+'.txt'
+
+
+if not os.path.isdir('data'):
+    os.mkdir('data')
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+def enable_reddit():
+    global reddit
+    
+    reddit = praw.Reddit(
+    client_id = os.getenv("client_id"),
+    client_secret = os.getenv("client_secret"),
+    username = os.getenv("reddit_username"),
+    password = os.getenv("password"),
+    user_agent = os.getenv("user_agent"),
+    check_for_updates=False,
+    comment_kind="t1",
+    message_kind="t4",
+    redditor_kind="t2",
+    submission_kind="t3",
+    subreddit_kind="t5",
+    trophy_kind="t6",
+    oauth_url="https://oauth.reddit.com",
+    reddit_url="https://www.reddit.com",
+    short_url="https://redd.it",
+    ratelimit_seconds=5,
+    timeout=16,
+    )
+
+
+def check_cached():
+    
+    if  os.path.exists(cache_file):
+
+        if (os.path.getmtime(cache_file)+ 3600*48 < float(time.time())):
+            os.remove(cache_file)
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
+
+
+
+
+
+def get_sumbissions():
+    
+    enable_reddit()
+
+    loaded_subreddit = reddit.subreddit(str(subreddit))
+    top = loaded_subreddit.hot(limit=500)
+    
+    return top
+
+
+def get_top_links():
+
+    submissions = get_sumbissions()
+
+    links = []
+
+    for submission in submissions:
+        link = get_link(submission)
+        if check_valid_image(link):
+            links.append(link)
+
+    return links
+
+
+
+def save_cache():
+
+    if  os.path.exists(cache_file):
+        os.remove(cache_file) 
+
+
+    links = get_top_links()
+
+    with open(cache_file, 'w') as listfile:
+        for link in links:
+            #print(link)
+            listfile.write('%s\n' % link)
+
+
+def load_cache():
+
+    links = []
+    with open(cache_file, 'r') as listfile:
+            for link in listfile:
+                current_link = link[:-1]
+                links.append(current_link)
+
+    return links
+
+
+
+def get_link(submission):
+    image_url = submission.url
+    #print(image_url)
+    return image_url
+
+
+def check_valid_image(image_url):
+    if 'i.redd' in image_url or 'i.imgur' in image_url:
+        if image_url[-4:] == '.jpg':
+            #print('valid img!')
+            return True
+        else:
+          #print('invalid img!')
+          return False
+
+
+def get_image():
+
+    if check_cached():
+        links = load_cache()
+    else:
+        save_cache()
+        links = load_cache()
+        
+    what_link = randint(1,len(links)-1)
+    link = links[what_link]
+    return link
+
+
+image = get_image()
+
+#print(image)
+
